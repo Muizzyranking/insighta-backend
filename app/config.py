181b21de5e1841d9c7c5b3129cf19cf53a60e466
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2026-04-29 13:17:40 +0100

    feat: app config

---
+++ app/config.py
@@ -0,0 +1,20 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+
+    app_env: str = "development"
+    secret_key: str
+
+    github_client_id: str
+    github_client_secret: str
+    github_redirect_uri: str
+
+    access_token_expire_minutes: int = 3
+    refresh_token_expire_minutes: int = 5
+
+    database_url: str = "sqlite+aiosqlite:///./insighta.db"
+
+
+settings = Settings()
